da33799b31989037f9fe250ec23b53203f4cfdff
Artem Shestakov <artem.s.shestakov@yandex.ru>
2024-02-29 18:50:00 +0300

    Update collector structure

--- internal/collector/widget.go
+++ internal/collector/widget.go
@@ -16,18 +16,18 @@ func (a AutoFAQCollector) collectWidgetsMetrics(ch chan<- prometheus.Metric) {
 			status, err := a.getWidgetStatus(service.Id, widgetId)
 			if err != nil {
 				success = 0
-				ch <- prometheus.MustNewConstMetric(a.WidgetStatus, prometheus.GaugeValue, 0, a.AutoFAQURL, service.Id, widgetId)
+				ch <- prometheus.MustNewConstMetric(a.widgetStatus, prometheus.GaugeValue, 0, a.AutoFAQURL, service.Id, widgetId)
 			}
 			if status == 200 {
-				ch <- prometheus.MustNewConstMetric(a.WidgetStatus, prometheus.GaugeValue, 1, a.AutoFAQURL, service.Id, widgetId)
+				ch <- prometheus.MustNewConstMetric(a.widgetStatus, prometheus.GaugeValue, 1, a.AutoFAQURL, service.Id, widgetId)
 			} else {
-				ch <- prometheus.MustNewConstMetric(a.WidgetStatus, prometheus.GaugeValue, 0, a.AutoFAQURL, service.Id, widgetId)
+				ch <- prometheus.MustNewConstMetric(a.widgetStatus, prometheus.GaugeValue, 0, a.AutoFAQURL, service.Id, widgetId)
 			}
 		}
 	}
 	duration := time.Since(begin)
-	ch <- prometheus.MustNewConstMetric(a.scrapeDurationDesc, prometheus.GaugeValue, duration.Seconds(), "widget_collector", a.AutoFAQURL)
-	ch <- prometheus.MustNewConstMetric(a.scrapeSuccessDesc, prometheus.GaugeValue, success, "widget_collector", a.AutoFAQURL)
+	ch <- prometheus.MustNewConstMetric(a.scrapeDurationDesc, prometheus.GaugeValue, duration.Seconds(), "autofaq_widget_collector", a.AutoFAQURL)
+	ch <- prometheus.MustNewConstMetric(a.scrapeSuccessDesc, prometheus.GaugeValue, success, "autofaq_widget_collector", a.AutoFAQURL)
 }
 
 func (a AutoFAQCollector) getWidgetStatus(serviceId, widgetId string) (int, error) {
@@ -37,23 +37,3 @@ func (a AutoFAQCollector) getWidgetStatus(serviceId, widgetId string) (int, erro
 	}
 	return resp.StatusCode, nil
 }
-
-// type WidgetCollector struct {
-// 	WidgetStatus *prometheus.Desc
-// }
-
-// func init() {
-// 	registerCollector("autofaq_widget", NewWidgetCollector)
-// }
-
-// func NewWidgetCollector() (Collector, error) {
-// 	return &WidgetCollector{
-// 		Status: prometheus.NewDesc("autofaq_widget_status", "Status of widget",
-// 			[]string{"site", "service-id", "widget-id"}, nil),
-// 	}, nil
-// }
-
-// func (c *WidgetCollector) Update(autofaq string, logger kitlog.Logger, ch chan<- prometheus.Metric) error {
-// 	fmt.Println(viper.Get("services"))
-// 	return nil
-// }
